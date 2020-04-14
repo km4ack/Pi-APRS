1b8763f487e5236016b52a1c562902ea29e62783
KM4ACK <jason.oleham@gmail.com>
2020-04-14 13:21:09 -0500

    fix paths

--- status.sh
+++ status.sh
@@ -4,8 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/config
 
-PKT="$MYCALL-$SSID>CQ,ARISS:>$MYCALL $STATUSCOMMENT"
-printf ${PKT} > $MSGPATH/status.txt
+
+#PKT="$MYCALL-$SSID>CQ,ARISS:>$MYCALL $STATUSCOMMENT"
+printf "${MYCALL}-${SSID}>CQ,ARISS:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
 
 
 #K4KDR-2>CQ,ARISS:>K4KDR Rasp-Pi APRS Station
