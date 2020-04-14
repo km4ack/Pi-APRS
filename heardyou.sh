e6a7772f6b91f4fc07e69e1ca9221495832bce88
KM4ACK <jason.oleham@gmail.com>
2020-04-14 12:37:37 -0500

    initial commit

---
+++ heardyou.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/config
+
+callsign=$1
+
+printf -v callsign %-9.9s "$callsign"
+
+printf "${MYCALL}-${SSID}>CQ,ARISS::${callsign}:${HEARDYOU} ${CITY}, ${STATE} ${GRID}" > $MSGPATH/heard.txt
+
+
+
