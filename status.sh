a9fa1a1b63824eaba92bc5230a6cf74e03e66421
KM4ACK <jason.oleham@gmail.com>
2020-04-15 18:01:03 -0500

    add path variable

--- status.sh
+++ status.sh
@@ -5,5 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/config
 
 
-#PKT="$MYCALL-$SSID>CQ,ARISS:>$MYCALL $STATUSCOMMENT"
-printf "${MYCALL}-${SSID}>CQ,ARISS:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
+
+#printf "${MYCALL}-${SSID}>CQ,ARISS:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
+printf "${MYCALL}-${SSID}>${APRSPATH}:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
