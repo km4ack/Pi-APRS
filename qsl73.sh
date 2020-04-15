f1058d2b887897d5742d0bcf4cf2ea86f552f6ce
KM4ACK <jason.oleham@gmail.com>
2020-04-15 18:00:54 -0500

    add path variable

--- qsl73.sh
+++ qsl73.sh
@@ -8,7 +8,7 @@ callsign=$1
 
 printf -v callsign %-9.9s "$callsign"
 
-printf "${MYCALL}-${SSID}>CQ,ARISS::${callsign}:${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
-
+#printf "${MYCALL}-${SSID}>CQ,ARISS::${callsign}:${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
+printf "${MYCALL}-${SSID}>${APRSPATH}::${callsign}:${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
 
 
