631e7d7f926fe70d6b9a45d18e1bea7b22e1b297
KM4ACK <jason.oleham@gmail.com>
2020-04-17 07:38:41 -0500

    convert call to uppercase

--- freemsg.sh
+++ freemsg.sh
@@ -6,12 +6,15 @@ source $DIR/config
 
 callsign=$1
 
+callsign=${callsign^^}
+
 printf -v callsign %-9.9s "$callsign"
 
 msg=$2
 
-#printf "${MYCALL}-$SSID>CQ,ARISS::${callsign}:${msg}" > $MSGPATH/free.txt
 printf "${MYCALL}-${SSID}>${APRSPATH}::${callsign}:${msg}" > $MSGPATH/free.txt
+
+#printf "${MYCALL}-${SSID}>${APRSPATH}::${callsign}:${msg}"
 
 
 
