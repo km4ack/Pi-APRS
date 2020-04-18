ff2552764adf60dfaa1e5f73d161ebe83a371f5a
KM4ACK <jason.oleham@gmail.com>
2020-04-18 07:44:54 -0500

    remove pi-aprs

--- status.sh
+++ status.sh
@@ -7,4 +7,4 @@ source $DIR/config
 touch $HOME/Pi-APRS/.msg
 
 #printf "${MYCALL}-${SSID}>CQ,ARISS:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
-printf "${MYCALL}-${SSID}>${APRSPATH}:>${MYCALL} Pi-APRS ${STATUSCOMMENT}" > $MSGPATH/status.txt
+printf "${MYCALL}-${SSID}>${APRSPATH}:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
