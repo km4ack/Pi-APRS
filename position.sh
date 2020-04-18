32a9c8ad8c235bb71413aacb6ea03bcda12c948c
KM4ACK <jason.oleham@gmail.com>
2020-04-18 07:16:32 -0500

    fix msg alert

--- position.sh
+++ position.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/config
 
+touch $HOME/Pi-APRS/.msg
+
 #PKT="$MYCALL-$SSID>CQ,ARISS:=$LATLON$SYMBOL $MYCALL $STATUSCOMMENT"
 PKT="$MYCALL-$SSID>$APRSPATH:=$LATLON$SYMBOL Pi-APRS $STATUSCOMMENT"
 echo $PKT > $MSGPATH/position.txt
