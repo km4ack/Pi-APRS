c15566ba0ea6439dc4dcbadf58135b6c0e2d9514
KM4ACK <jason.oleham@gmail.com>
2020-04-18 07:44:47 -0500

    remove pi-aprs

--- qsl73.sh
+++ qsl73.sh
@@ -11,6 +11,6 @@ callsign=$1
 printf -v callsign %-9.9s "$callsign"
 
 #printf "${MYCALL}-${SSID}>CQ,ARISS::${callsign}:${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
-printf "${MYCALL}-${SSID}>${APRSPATH}::${callsign}:Pi-ARPS ${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
+printf "${MYCALL}-${SSID}>${APRSPATH}::${callsign}:${QSLMESSAGE} ${MYCALL} in ${GRID}" > $MSGPATH/qsl.txt
 
 
