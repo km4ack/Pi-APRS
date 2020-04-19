32a055cb9f2c19a26ad0f09ab78ed5bd613206d3
KM4ACK <jason.oleham@gmail.com>
2020-04-19 09:38:18 -0500

    update flag path

--- position.sh
+++ position.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/config
 
-touch /run/user/1000/pi-aprs/.msg
+touch /run/user/1000/.msg
 
 #PKT="$MYCALL-$SSID>CQ,ARISS:=$LATLON$SYMBOL $MYCALL $STATUSCOMMENT"
 PKT="$MYCALL-$SSID>$APRSPATH:=$LATLON$SYMBOL $STATUSCOMMENT"
