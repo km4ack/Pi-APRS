565470543fda61390590d40745d82251f88c36a2
KM4ACK <jason.oleham@gmail.com>
2022-04-12 13:09:03 -0500

    replace 1000 with 1000

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/config
 
-touch /run/user/1000/.msg
+touch /run/user/$UID/.msg
 
 #printf "${MYCALL}-${SSID}>CQ,ARISS:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
 printf "${MYCALL}-${SSID}>${APRSPATH}:>${MYCALL} ${STATUSCOMMENT}" > $MSGPATH/status.txt
