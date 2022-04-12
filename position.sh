c82364a97de51b4123979d4dc8d0fd0191315d8c
KM4ACK <jason.oleham@gmail.com>
2022-04-12 13:08:44 -0500

    replace 1000 with 1000

--- position.sh
+++ position.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/config
 
-touch /run/user/1000/.msg
+touch /run/user/$UID/.msg
 
 #PKT="$MYCALL-$SSID>CQ,ARISS:=$LATLON$SYMBOL $MYCALL $STATUSCOMMENT"
 PKT="$MYCALL-$SSID>$APRSPATH:=$LATLON$SYMBOL $STATUSCOMMENT"
